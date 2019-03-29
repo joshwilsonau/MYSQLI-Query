65c0ec58ac3b5e7df183ba4edb97f1f46dc01875
Josh Wilson <team@ittas.com.au>
2019-03-29 13:38:43 +1100

    Update query.php

--- query.php
+++ query.php
@@ -3,11 +3,8 @@ $query = "SELECT * FROM table";
 $queryname = mysqli_query($connection, $query); {
 
   while($row = mysqli_fetch_assoc($queryname)) {
-    $site_title = $row['site_title'];
-    $site_desc = $row['site_description'];
-    $site_key = $row['site_keywords'];
-    $site_theme = $row['site_theme'];
-    $date = $row['site_date'];
+   // $var = $row['collumn name'];
+    
   }
 }
 ?>
